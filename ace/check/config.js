5ddbd9e0163d3e5a3e2855c210277727e72dc490
takraw369 <takraw501@gmail.com>
2026-08-23 19:39:04 +0900

    feat: add ACE public route config

---
+++ ace/check/config.js
@@ -0,0 +1,10 @@
+window.ACE_CONFIG = {
+  liffId: "2009239912-4mV8q3Zp",
+  harnessBaseUrl: "https://sunlovesflow-line.masafuminoda.workers.dev",
+  formIds: {
+    BODY: "",
+    COGNITION: "",
+    EMOTION: "",
+    ACTION: ""
+  }
+};
